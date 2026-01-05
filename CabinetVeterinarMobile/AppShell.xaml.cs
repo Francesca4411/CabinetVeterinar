a9ef0bbf867cfefd3d654a937d7ed391bfbd170a
Gulei Francesca <francesca.gulei@gmail.com>
2026-01-05 16:58:33 +0200

    Add project files.

---
+++ CabinetVeterinarMobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CabinetVeterinarMobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
